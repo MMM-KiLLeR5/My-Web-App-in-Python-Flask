7322a425aa57c00b2696f8a5f0dee4920af858b8
dadabaev raufjon <dadabaev.ra@phystech.edu>
2024-05-12 00:20:34 +0300

    changed constants names in user_api

--- src/blueprints/user_api/__init__.py
+++ src/blueprints/user_api/__init__.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import decode_token
 from src.schemas.schemas import UserSchema, AuthSchema, ShareSchema, DataSchema
 from src.Authorization.Authorization import Authorization as Auth
 from src.User.HandleUser import HandleUser
+from src.Constants import UserErrorMessages, UserApprovalMessages
 
 user = Blueprint('user', __name__, url_prefix='/user_api')
 
@@ -24,13 +25,13 @@ def protected():
 @use_kwargs(UserSchema)
 def user_register(**kwargs):
     if HandleUser.is_user('username', kwargs['username']):
-        return {"error": "User not found"}, 411
+        return {"error": UserErrorMessages.NON_EXISTING_USER}, 411
     if HandleUser.is_user('passport_id', kwargs['passport_id']):
-        return {"error": "User not found"}, 409
+        return {"error": UserErrorMessages.NON_EXISTING_USER}, 409
     if HandleUser.is_user('phone_number', kwargs['phone_number']):
-        return {"error": "User not found"}, 410
+        return {"error": UserErrorMessages.NON_EXISTING_USER}, 410
     HandleUser.create_user_account(**kwargs)
-    return {'message': 'good'}, 200
+    return {'message': UserApprovalMessages.REGISTER_USER_SUCCESSFULLY}, 200
 
 
 @user.route('/login', methods=['POST'])
@@ -39,7 +40,7 @@ def user_register(**kwargs):
 def user_login(**kwargs):
     ans = Auth.user_login(**kwargs)
     if ans is None:
-        return {"error": "User not found"}, 401
+        return {"error": UserErrorMessages.NON_EXISTING_USER}, 401
     token = HandleUser.get_token(ans)
     return {'access_token': token}
 
@@ -57,10 +58,10 @@ def data_of_users(username):
 def share_gb(username, **kwargs):
     res = HandleUser.share_gb_with_friend(username, **kwargs)
     if res is None:
-        return {"error": "There is no user with this phone_number"}, 409
+        return {"error": UserErrorMessages.NON_EXISTING_USER_WITH_PHONE_NUMBER}, 409
     if not res:
-        return {"error": "Not enough gb"}, 410
-    return {'message': 'good'}, 200
+        return {"error": UserErrorMessages.NOT_ENOUGH_GB}, 410
+    return {'message': UserApprovalMessages.GB_SHARED_SUCCESSFULLY}, 200
 
 
 @user.route('/share_minute/<string:username>', methods=['POST'])
@@ -68,10 +69,10 @@ def share_gb(username, **kwargs):
 def share_minute(username, **kwargs):
     res = HandleUser.share_minute_with_friend(username, **kwargs)
     if res is None:
-        return {"error": "There is no user with this phone_number"}, 409
+        return {"error": UserErrorMessages.NON_EXISTING_USER_WITH_PHONE_NUMBER}, 409
     if not res:
-        return {"error": "Not enough minute"}, 410
-    return {'message': 'good'}, 200
+        return {"error": UserErrorMessages.NOT_ENOUGH_MINUTE}, 410
+    return {'message': UserApprovalMessages.MINUTE_SHARED_SUCCESSFULLY}, 200
 
 
 @user.route('/buy_gb/<string:username>', methods=["POST"])
@@ -79,8 +80,8 @@ def share_minute(username, **kwargs):
 def buy_gb(username, value):
     res = HandleUser.handle_buy_gb(username, value)
     if not res:
-        return {"error": "Not enough money"}, 406
-    return {'message': 'Successed'}, 200
+        return {"error": UserErrorMessages.NOT_ENOUGH_MONEY}, 406
+    return {'message': UserApprovalMessages.GB_BOUGHT_SUCCESSFULLY}, 200
 
 
 @user.route('/buy_minute/<string:username>', methods=["POST"])
@@ -88,23 +89,23 @@ def buy_gb(username, value):
 def buy_minute(username, value):
     res = HandleUser.handle_buy_minute(username, value)
     if not res:
-        return {"error": "Not enough money"}, 406
-    return {'message': 'Successed'}, 200
+        return {"error": UserErrorMessages.NOT_ENOUGH_MONEY}, 406
+    return {'message': UserApprovalMessages.MINUTE_BOUGHT_SUCCESSFULLY}, 200
 
 
 @user.route('/deposit/<string:username>', methods=["POST"])
 @use_kwargs(ShareSchema(only=['value']))
 def deposit(username, value):
     HandleUser.deposit_money(username, value)
-    return {'message': 'Successed'}, 200
+    return {'message': UserApprovalMessages.DEPOSIT_SUCCESSFULLY}, 200
 
 
 @user.route('/pay_tariff/<string:username>', methods=["POST"])
 def pay_tariff(username):
     res = HandleUser.user_pay_tariff(username)
     if not res:
-        return {"error": "Not enough money"}, 406
-    return {'message': 'Successed'}, 200
+        return {"error": UserErrorMessages.NOT_ENOUGH_MONEY}, 406
+    return {'message': UserApprovalMessages.PAID_TARIFF_SUCCESSFULLY}, 200
 
 
 @user.route('/change_tariff/<string:username>', methods=["POST"])
@@ -112,13 +113,13 @@ def pay_tariff(username):
 def change_tariff(username, tariff_id):
     res = HandleUser.change_tariff(username, tariff_id)
     if res:
-        return {"error": "Not such tariff"}, 406
-    return {'message': 'Successed'}, 200
+        return {"error": UserErrorMessages.NON_EXISTING_TARIFF}, 406
+    return {'message': UserApprovalMessages.CHANGE_TARIFF_SUCCESSFULLY}, 200
 
 @user.route('/data_of_users/<string:phone_number>', methods=['GET'])
 @marshal_with(DataSchema)
 def get_data_by_phone_number(phone_number):
     my_user = HandleUser.show_user_details('', phone_number)
     if my_user is None:
-        return {"error": "There is no such user"}, 406
+        return {"error": UserErrorMessages.NON_EXISTING_USER}, 406
     return my_user
